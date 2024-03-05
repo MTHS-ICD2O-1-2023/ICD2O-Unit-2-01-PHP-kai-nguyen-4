edc9eb33a9fdda2c563f5e91e44cf94bcca9c35b
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-05 19:48:56 +0000

    website with php

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<html lang="en-ca">
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="XXX page program" />
+  <meta name="author" content="Kai Nguyen" />
+  <meta name="keywords" content="mths, ics2o" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Js interactivity with PHP</title>
+</head>
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <script src="./js/script.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">Js Interactivity with PHP</span>
+        <!-- Add spacer, to align navigation to the right -->
+        <div class="mdl-layout-spacer"></div>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/Opera Snapshot_2024-03-03_223912_www.flickr.com.png" alt="volleyball image" />
+      </div>
+      <br />
+      <br />
+      <div class="page-content-answer">
+        <div id="answer">
+          <?php echo '<p>Hello, World!</p>'; ?>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
 </html>
